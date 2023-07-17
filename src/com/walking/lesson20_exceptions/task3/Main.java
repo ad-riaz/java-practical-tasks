83fed93c446aa654e82d2810059773819b76c3b1
Aleksandr Riazantsev <ad.riazantsev@gmail.com>
2023-07-17 09:34:48 +0300

    Урок 20. «Исключения». Задание 3.

--- src/com/walking/lesson20_exceptions/task3/Main.java
+++ src/com/walking/lesson20_exceptions/task3/Main.java
@@ -1,5 +1,12 @@
 package com.walking.lesson20_exceptions.task3;
 
+import com.walking.lesson20_exceptions.task3.exception.ArrayValidationException;
+import com.walking.lesson20_exceptions.task3.exception.UnknownAnimalException;
+import com.walking.lesson20_exceptions.task3.model.Animal;
+import com.walking.lesson20_exceptions.task3.model.Cat;
+import com.walking.lesson20_exceptions.task3.model.Cow;
+import com.walking.lesson20_exceptions.task3.model.Dog;
+
 /**
  * Реализуйте любой из вариантов задачи в уроке 18.
  * Для ситуации, когда тип животного неизвестен, выбрасывайте собственное исключение UnknownAnimalException.
@@ -11,5 +18,30 @@ package com.walking.lesson20_exceptions.task3;
  */
 public class Main {
     public static void main(String[] args) {
+        Animal[] animals = new Animal[]{new Cat(), new Dog(), new Cow(), null};
+
+        soundAll(animals);
+    }
+
+    private static void soundAll(Animal[] animals) {
+        for (int i = 0; i < animals.length; i++) {
+            if (animals[i] == null) {
+                throw new ArrayValidationException(i);
+            }
+
+            sound(animals[i]);
+        }
+    }
+
+    private static void sound(Animal animal) {
+        if (animal.getClass().equals(Cat.class)) {
+            ((Cat) animal).meow();
+        } else if (animal.getClass().equals(Dog.class)) {
+            ((Dog) animal).woof();
+        } else if (animal.getClass().equals(Cow.class)) {
+            ((Cow) animal).moo();
+        } else {
+            throw new UnknownAnimalException("Unknown animal");
+        }
     }
 }
