f6181f12c995e9ab116b2c9c2cb83d47060168cf
Aleksandr Riazantsev <ad.riazantsev@gmail.com>
2023-07-24 10:53:15 +0300

    Урок 26. «Классы для работ со строками». Задание 2.

--- src/com/walking/lesson26_string_types/task2/Main.java
+++ src/com/walking/lesson26_string_types/task2/Main.java
@@ -1,5 +1,7 @@
 package com.walking.lesson26_string_types.task2;
 
+import java.util.Scanner;
+
 /**
  * Реализуйте программу, принимающую с клавиатуры строку,
  * содержащую слова, разделенные пробелом.
@@ -12,5 +14,33 @@ package com.walking.lesson26_string_types.task2;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите вашу строку: ");
+        String[] words = scanner.nextLine()
+                                .trim()
+                                .toLowerCase()
+                                .split(" ");
+        scanner.close();
+        int counter = 0;
+
+        for (int i = 0; i < words.length; i++) {
+
+            boolean isUnique = true;
+            int j = 0;
+
+            while (j < i && isUnique) {
+                if (words[i].equals(words[j])) {
+                    isUnique = false;
+                } else {
+                    j++;
+                }
+            }
+
+            if (isUnique) {
+                counter++;
+            }
+        }
+
+        System.out.println(counter);
     }
 }
